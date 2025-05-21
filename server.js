666d20b5cb220e9680f3252b5846615fa8c8cd0d
Asish-2004-ai <asishdalabehera375@gmail.com>
2025-05-21 16:55:23 +0530

    changed

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is live!");
+});
+
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/profile", require("./routes/profileRoutes"));
 app.use("/api/jobs", require("./routes/jobRoutes"));
